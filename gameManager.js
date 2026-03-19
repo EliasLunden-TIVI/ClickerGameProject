dc8fedbcafbffdd749fc17f9e0e3519aa2edda28
N3xu_ss <53103@edu.raseko.fi>
2026-03-19 10:22:29 +0200

    Added Upgrade cost display and created autoclick framework.

--- gameManager.js
+++ gameManager.js
@@ -8,19 +8,27 @@ let ClickPower = 1;
 
 // AutoClickDelay is how long the delay is for automatic clicks //
 
-let AutoClickDelay = 1;
+let AutoClickDelay = 5;
 
+// Starting costs for upgrades
 
-
-let PowerUpgradeCost = 50;
-
+let ClickPowerUpgradeCost = 25; 
+let AutoClickUpgradeCost = 10;
+// GUI elements //
 const scoreDisplay = document.getElementById("ScoreLabel");
 const clickButton = document.getElementById("ClickButton");
+// Click power Upgrades //
 const clickPowerButton = document.getElementById("ClickPowerButton");
+const clickPowerButtonPrice = document.getElementById("ClickPowerButtonPrice");
+// Auto Click Upgrades //
+const AutoClickButton = document.getElementById("AutoClickButton");
+const AutoClickButtonPrice = document.getElementById("AutoClickButtonPrice");
 
+// Set prices on start //
+ClickPowerButtonPrice.textContent = `Cost: ${ClickPowerUpgradeCost}`;
+AutoClickButtonPrice.textContent = `Cost: ${ClickPowerUpgradeCost}`;
 
 // Function that adds score for button press //
-
 function pressButton(power) {
     Clickpoints += power;
     scoreDisplay.textContent = `Points: ${Clickpoints}`;
@@ -32,24 +40,52 @@ function upgradeClickPower(power) {
 
 }
 
+function UpgradeAutoClick(reduction) {
+    AutoClickDelay -= reduction;
+    scoreDisplay.textContent = `Points: ${Clickpoints}`;
+    AutoClick(reduction)
+}
+
 clickButton.addEventListener("click", function () {
     pressButton(ClickPower);
 });
 
 clickPowerButton.addEventListener("click", function () {
-    if (Clickpoints >= PowerUpgradeCost) {
+    if (Clickpoints >= ClickPowerUpgradeCost) {
         
         // Deduct the cost of the upgrade //
-        Clickpoints -= PowerUpgradeCost
+        Clickpoints -= ClickPowerUpgradeCost
 
         upgradeClickPower(1)
 
-        // The price of the upgrade doubles //
-        PowerUpgradeCost *= 2
+        // The price of the upgrade increases by 50% //
+        ClickPowerUpgradeCost += ClickPowerUpgradeCost / 2 
+        clickPowerButtonPrice.textContent = `Cost: ${ClickPowerUpgradeCost}`;
     } else { console.log("Not enough money for upgrade")}
         
 });
 
+AutoClickButton.addEventListener("click", function () {
+    if (Clickpoints >= AutoClickUpgradeCost) {
 
+        UpgradeAutoClick(1)
 
-pressButton(ClickPower)
+        // The price of the upgrade increases by 50% //
+        AutoClickUpgradeCost += AutoClickUpgradeCost / 2 
+        AutoClickButtonPrice.textContent = `Cost: ${AutoClickButtonPrice}`;
+        
+    }
+});
+// This script is broken and causes a infinite loop //
+function AutoClick(power) {
+    while (AutoClickDelay > 0) {
+        if (AutoClickDelay > 0) {
+            setTimeout(power)
+            pressButton()
+        } else {
+            break
+        }
+        
+    } 
+    
+}
